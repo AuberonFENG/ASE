369d29b1cc3c6b78a5df497dd3218aeab54ed1c8
JerryZ <zhangr8@tcd.ie>
2024-11-30 14:29:10 +0000

    [feature]: Add file upload feature

--- VRMeetingRoom/Assets/Scripts/Request/PlayFabSignInRequest.cs
+++ VRMeetingRoom/Assets/Scripts/Request/PlayFabSignInRequest.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayFabSignInRequest : MonoBehaviour
 {
+   public GameObject Canvas;
    public PlayFabManager playerFabManager;
    public void GetUserInfo(string username,string password)
    {
       playerFabManager.RegisterPlayer(username,password);
+      Canvas.gameObject.SetActive(false);
    }
 }
