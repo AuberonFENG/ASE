dd95974d44032d3e3ab85d176dddeff39e2b88e8
JerryZ <zhangr8@tcd.ie>
2025-03-24 13:24:38 +0000

    [UI]:change UI

--- VRMeetingRoom/Assets/MainScripts/Request/PlayFabLogInRequest.cs
+++ VRMeetingRoom/Assets/MainScripts/Request/PlayFabLogInRequest.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayFabLogInRequest : MonoBehaviour
 {
     public PlayFabManager playerFabManager; 
-    public GameObject MeetingChoice;
     public void GetUserInfo(string username,string password)
     {
         playerFabManager.Login(username,password);
         this.gameObject.SetActive(false);
-        MeetingChoice.gameObject.SetActive(true);
+        SceneManager.LoadScene("Main");
+        SceneManager.UnloadSceneAsync("UI1");
     }
 }
