04737433bf4db7d388af514e78ed75bcf0516adf
Ziao <unix64a@gmail.com>
2025-03-28 16:40:39 +0000

    fix look around

--- VRMeetingRoom/Assets/DisableTrackedPose.cs
+++ VRMeetingRoom/Assets/DisableTrackedPose.cs
@@ -4,17 +4,23 @@ using UnityEngine.InputSystem.XR; // Needed to reference TrackedPoseDriver
 public class DisableTrackedPoseDriver : MonoBehaviour
 {
     [SerializeField] private GameObject leftController;
+    private TrackedPoseDriver trackedPoseDriver;
     void Start()
     {
+
         // Find the TrackedPoseDriver component on this GameObject
-        TrackedPoseDriver trackedPoseDriver = GetComponent<TrackedPoseDriver>();
+        trackedPoseDriver = GetComponent<TrackedPoseDriver>();
+    }
+
+    void Update()
+    {
 
         // Check
-        if (!leftController.activeInHierarchy)
+        if (leftController.activeInHierarchy)
         {
             if (trackedPoseDriver != null)
             {
-                trackedPoseDriver.enabled = false;
+                trackedPoseDriver.enabled = true;
             }
         }
     }
